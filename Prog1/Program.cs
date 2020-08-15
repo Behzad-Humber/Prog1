04dae5864dc6421c3703db00cf9102ab46577bea
behzad-humber <joulaei@yahoo.com>
2020-08-15 10:24:59 -0400

    Commit3-to update message

---
+++ Prog1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+namespace generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public class GenericList<T> where T : struct
+        {
+            List<T> _list = new List<T>();
+            public void add(T item)
+            {
+                _list.Add(item);
+            }
+            public T get(int index)
+            {
+                if (index < _list.Count)
+                {
+                    return _list[index];
+                }
+                else
+                {
+                    throw new System.ArgumentOutOfRangeException ;
+                }
+
+            }
+
+            public Collection<T> Order_Desc()
+            {
+
+                return _list.
+            }
+        }
+    }
+}
